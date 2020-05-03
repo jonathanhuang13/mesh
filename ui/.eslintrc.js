fbe0d21dfb3fbddeb07f8ee55a7d3f0149fda5dd
jonathanhuang13 <jonathan.huang1@gmail.com>
2020-05-02 19:38:55 -0700

    Add react eslint

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
   parserOptions: {
     project: 'tsconfig.json',
     sourceType: 'module',
@@ -8,12 +8,13 @@ module.exports = {
       jsx: true,
     },
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   rules: {
     '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
     '@typescript-eslint/adjacent-overload-signatures': 'error',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-unused-vars': ['off', { argsIgnorePattern: '^_' }],
+    'react-hooks/exhaustive-deps': 'warn',
     'no-redeclare': 'error',
     'no-return-await': 'error',
     'no-duplicate-case': 'error',
