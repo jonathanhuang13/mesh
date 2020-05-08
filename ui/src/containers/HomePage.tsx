e1fdbe8ac8fd8ef58926af72f1bdbb7a694d2434
jonathanhuang13 <jonathan.huang1@gmail.com>
2020-05-08 08:28:06 -0700

    Remove and rename files for cleanup

--- ui/src/containers/HomePage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { RouteProps } from 'react-router-dom';
-
-import Home from '../components/Home';
-
-export default function HomePage(_: RouteProps): JSX.Element {
-  return <Home />;
-}
--- ui/src/pages/Home/index.tsx
+++ ui/src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import routes from '../../constants/routes';
-import NotesSideBar from '../NotesSideBar';
+import NotesSideBar from '../../components/NotesSideBar';
 import styles from './styles.scss';
 
 export default function Home(): JSX.Element {
