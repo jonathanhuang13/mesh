baac4336c45feb7300d16f328646971c073958db
jonathanhuang13 <jonathan.huang1@gmail.com>
2020-05-08 08:17:56 -0700

    Separate queries

--- ui/src/components/NotesSideBar/index.tsx
+++ ui/src/components/NotesSideBar/index.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 
 import styles from './styles.scss';
+import { Note, LIST_NOTES_QUERY, ListParams } from '../../apollo-client/notes';
 
 export interface Props {}
 
-const getNotesQuery = gql`
-  query getNotes {
-    notes {
-      id
-    }
-  }
-`;
-
 export default function NotesSideBar(_props: Props): JSX.Element {
-  const { loading, error, data } = useQuery(getNotesQuery);
+  const { loading, error, data } = useQuery<Note[], ListParams>(LIST_NOTES_QUERY);
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>error...</p>;
+  if (error) return <p>{`error: ${error.message}`}</p>;
 
   console.log(data);
 
